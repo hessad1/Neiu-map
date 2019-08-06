9c23ddb1a74087932d6d07f03d564290c896322a
student <student@ELC-MAC-AIR-09.local>
2019-08-06 16:46:15 -0500

    all files

---
+++ main.py
@@ -0,0 +1,20 @@
+#main.py
+# the import section
+import webapp2
+import jinja2
+import os
+
+the_jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+# the handler section
+class MainPage(webapp2.RequestHandler):
+    def get(self): #for a get request
+        welcome_template = the_jinja_env.get_template('Documents/Map.html')
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(welcome_template.render()) #the response
+        
+# the app configuration section
+app = webapp2.WSGIApplication([('/',MainPage),], debug=True)
