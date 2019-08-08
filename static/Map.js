b52431842c3cd804da8d8cace43f437186dc19c1
student <student@ELC-MAC-AIR-14.local>
2019-08-08 10:40:02 -0500

    ADD SVG FUNCTIONALITY

--- static/Map.js
+++ static/Map.js
@@ -1,4 +1,33 @@
-function myFunction(selector) {
-    popup = document.getElementById("Svg_Pop" + selector); 
-    popup.classList.toggle("show");
+function MakeBlue(selector) {
+    if (selector === "ath4630") {
+        var Fi = "p" + selector;
+        var svgObject = document.getElementById("InnerCity").contentDocument;
+    console.log(svgObject);
+    var selectedPath = svgObject.getElementById(Fi);
+    if (selectedPath.style.opacity === "0"){
+        selectedPath.style.opacity = ".75";
+    }else{
+        selectedPath.style.opacity = "0"
+    }
+    }else if(selector === "ath817"){
+        var Fi = "p" + selector;
+        var svgObject = document.getElementById("Elcentro").contentDocument;
+    console.log(svgObject);
+    var selectedPath = svgObject.getElementById(Fi);
+    if (selectedPath.style.opacity === "0"){
+        selectedPath.style.opacity = ".75";
+    }else{
+        selectedPath.style.opacity = "0"
+    }
+    }else {
+    var Fi = "p" + selector; 
+    var svgObject = document.getElementById("MAP").contentDocument;
+    console.log(svgObject);
+    var selectedPath = svgObject.getElementById(Fi);
+    if (selectedPath.style.opacity === "0"){
+        selectedPath.style.opacity = ".75";
+    }else{
+        selectedPath.style.opacity = "0"
+    }
+}
 }
