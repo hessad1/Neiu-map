87a0c820ca27b8a3a95bbb0e8979bd0f534d0c83
student <student@ELC-MAC-AIR-14.local>
2019-08-08 10:58:57 -0500

    fixed buissness buildigng bug

--- static/Map.js
+++ static/Map.js
@@ -1,33 +1,30 @@
 function MakeBlue(selector) {
-    if (selector === "ath4630") {
-        var Fi = "p" + selector;
-        var svgObject = document.getElementById("InnerCity").contentDocument;
-    console.log(svgObject);
-    var selectedPath = svgObject.getElementById(Fi);
-    if (selectedPath.style.opacity === "0"){
-        selectedPath.style.opacity = ".75";
-    }else{
-        selectedPath.style.opacity = "0"
-    }
-    }else if(selector === "ath817"){
-        var Fi = "p" + selector;
-        var svgObject = document.getElementById("Elcentro").contentDocument;
-    console.log(svgObject);
-    var selectedPath = svgObject.getElementById(Fi);
-    if (selectedPath.style.opacity === "0"){
-        selectedPath.style.opacity = ".75";
-    }else{
-        selectedPath.style.opacity = "0"
-    }
-    }else {
-    var Fi = "p" + selector; 
-    var svgObject = document.getElementById("MAP").contentDocument;
-    console.log(svgObject);
-    var selectedPath = svgObject.getElementById(Fi);
-    if (selectedPath.style.opacity === "0"){
-        selectedPath.style.opacity = ".75";
-    }else{
-        selectedPath.style.opacity = "0"
-    }
-}
+	if (selector === "ath4630") {
+		var Fi = "p" + selector;
+		var svgObject = document.getElementById("InnerCity").contentDocument;
+		var selectedPath = svgObject.getElementById(Fi);
+		if (selectedPath.style.opacity === "0") {
+			selectedPath.style.opacity = ".75";
+		} else {
+			selectedPath.style.opacity = "0"
+		}
+	} else if (selector === "ath817") {
+		var Fi = "p" + selector;
+		var svgObject = document.getElementById("Elcentro").contentDocument;
+		var selectedPath = svgObject.getElementById(Fi);
+		if (selectedPath.style.opacity === "0") {
+			selectedPath.style.opacity = ".75";
+		} else {
+			selectedPath.style.opacity = "0"
+		}
+	} else {
+		var Fi = "p" + selector;
+		var svgObject = document.getElementById("MAP").contentDocument;
+		var selectedPath = svgObject.getElementById(Fi);
+		if (selectedPath.style.opacity === "0") {
+			selectedPath.style.opacity = ".75";
+		} else {
+			selectedPath.style.opacity = "0"
+		}
+	}
 }
